65595ded8873081334786173428e8ce79c328506
Yaroslav <yaroslav.kirnos@gmail.com>
2025-02-13 18:58:03 +0200

    Домашка

---
+++ vehicles2.py
@@ -0,0 +1,9 @@
+from vehicles import Car, Bike, Truck
+
+car = Car("Toyota", "Corolla", 4)
+bike = Bike("Trek", "Marlin 7", "гірський")
+truck = Truck("Volvo", "FH16", 20000)
+
+print(car.info())
+print(bike.info())
+print(truck.info())
